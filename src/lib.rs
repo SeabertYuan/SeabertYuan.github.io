f733e31358e5a2147eb83a8a27f37a6ade7a583d
SeabertYuan <seabert.s.yuan23z@gmail.com>
2026-03-29 00:24:04 -0400

    replace js with rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
 use wasm_bindgen::prelude::*;
+use gloo::events::EventListener;
+use web_sys::Element;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+const QUOTES: [&str; 6] = [
+    "so... the weather's nice?",
+    "live life without regrets.",
+    "two broken hearts won't mend each other.",
+    "1 Corinthians 13:4 | Love is patient, love is kind. It does not envy, it does not boast, it is not proud.",
+    "home-made",
+    "the race does not end after you cross the finish line.",
+];
 
 // Called when the Wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -9,12 +26,28 @@ fn main() -> Result<(), JsValue> {
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    init_nav_burger(&document)?.forget();
 
-    body.append_child(&val)?;
+    init_hero_quote(&document)?;
 
+    Ok(())
+}
+
+fn init_nav_burger(document: &web_sys::Document) -> Result<EventListener, JsValue> {
+    let nav_links = document.get_element_by_id("navLinks").expect("navLinks will exist");
+    let burger = document.get_element_by_id("navBurger").expect("navLinks will exist");
+    Ok(EventListener::new(&burger.clone(), "click", move |_event| {
+        nav_links.class_list().toggle("open");
+
+        burger.class_list().toggle("toggle");
+    }))
+}
+
+fn init_hero_quote(document: &web_sys::Document) -> Result<(), JsValue> {
+    let subtitle = document.get_element_by_id("mainSubtitle").expect("subtitle will exist");
+    let rand: usize = getrandom::u32().map_err(|e| JsValue::from_str(e.to_string().as_str()))? as usize;
+    let i = rand % QUOTES.len();
+    subtitle.set_text_content(Some(QUOTES[i]));
     Ok(())
 }
 
